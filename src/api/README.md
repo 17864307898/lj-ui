562d42b054c6bd9114e58644b0bb42d4cb53e0d1
杨雅婷 <yangyating@7-cai.com>
2022-11-22 17:28:49 +0800

    feat(utils): 轮询 验证码

--- src/api/README.md
+++ src/api/README.md
@@ -25,7 +25,7 @@ import { deepClone } from 'lj-design/es/utils'
 | byteConvert | 字节单位转换 | `bytes` _number_  _string_ | _string_ |
 | paramObj | 将url请求参数转为json格式 | `url` _string_ | _object_ |
 | randomNumber | m到n的随机数 | `m`_number_ `n`_number_ | _number_ |
-|  |  |  |  |
+|  countDown|  验证码的倒计时|  `a` this `b`_number_ 倒计时的时长 `c` _String_ 需要倒计时的参数名字|  |
 |  |  |  |  |
 |  |  |  |  |
 ### 时间处理
@@ -36,3 +36,11 @@ import { deepClone } from 'lj-design/es/utils'
 |  |  |  |  |
 |  |  |  |  |
 |  |  |  |  |
+
+### 轮询处理
+
+| 方法名 | 说明 | 参数 | 返回值 |
+|--------|------|-----|-------|
+|  Thread|  轮询类名 | new Thread({ start()=>{},stop()=>{},number: 0, time: 300}) | `start`轮询中;`stop`轮询手动结束;`number`: 轮询次数配置，不配置默认无线轮询;`time`: 轮询的时间,不配置默认300ms|
+|  thread.run()|  开始轮询|  |  |
+|  thread.stop()|  主动结束轮询 |
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unsafe-finally */
+/* eslint-disable radix */
 /**
  * session 单次缓存
  * @param {*} key session key值
@@ -5,15 +7,15 @@
  * @returns 仅在不传入val值时返回当前key值对应的session值
  */
 export function onceSession(key, val) {
-  if (arguments.length === 0) return
+  if (arguments.length === 0) return;
 
   if (val || val === 0) {
-    sessionStorage.setItem(key, val)
+    sessionStorage.setItem(key, val);
   } else {
-    const val = sessionStorage.getItem(key)
-    sessionStorage.removeItem(key)
+    const val = sessionStorage.getItem(key);
+    sessionStorage.removeItem(key);
 
-    return val
+    return val;
   }
 }
 
@@ -21,47 +23,47 @@ export function onceSession(key, val) {
  * 深拷贝
  * @param {Object} target 源数据
  * @returns {Array, Object} 深拷贝后的数据
- * */ 
- export const deepClone = (target) => {
-  function _deepClone (_target) {
-    if (_target instanceof Date) return new Date(_target)
-    if (_target instanceof RegExp) return new RegExp(_target)
-    if (typeof _target !== 'object' || !_target) return _target
+ * */
+export const deepClone = (target) => {
+  function _deepClone(_target) {
+    if (_target instanceof Date) return new Date(_target);
+    if (_target instanceof RegExp) return new RegExp(_target);
+    if (typeof _target !== 'object' || !_target) return _target;
 
-    let obj = {}
+    let obj = {};
 
-    if (_target instanceof Array) obj = []
+    if (_target instanceof Array) obj = [];
 
     Object.keys(_target).forEach((key) => {
-      obj[key] = _deepClone(_target[key])
-    })
+      obj[key] = _deepClone(_target[key]);
+    });
 
-    return obj
+    return obj;
   }
 
   return _deepClone(target);
-}
+};
 
 /**
  * 移除对象中为空的属性
  * @param {*} object
  * @returns
  */
- export function cleanObjectEmpty(object) {
+export function cleanObjectEmpty(object) {
   Object.entries(object).forEach(([k, v]) => {
     if (v && typeof v === 'object') {
       if (Object.prototype.toString.call(v) === '[object Array]') {
         if (!v.length) {
-          delete object[k]
+          delete object[k];
         }
       }
-      cleanObjectEmpty(v)
+      cleanObjectEmpty(v);
     }
     if (!v && v !== 0) {
-      delete object[k]
+      delete object[k];
     }
-  })
-  return object
+  });
+  return object;
 }
 
 /**
@@ -69,23 +71,23 @@ export function onceSession(key, val) {
  * @param number
  * @returns {*}
  */
- export function byteConvert(bytes) {
-  if (isNaN(bytes)) return ''
+export function byteConvert(bytes) {
+  if (isNaN(bytes)) return '';
 
-  const symbols = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-  let exp = Math.floor(Math.log(bytes)/Math.log(2))
+  const symbols = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  let exp = Math.floor(Math.log(bytes) / Math.log(2));
 
   if (exp < 1) {
-      exp = 0
+    exp = 0;
   }
-  const i = Math.floor(exp / 10)
-  bytes /= 2 ** (10 * i)
+  const i = Math.floor(exp / 10);
+  bytes /= 2 ** (10 * i);
 
   if (bytes.toString().length > bytes.toFixed(2).toString().length) {
-      bytes = bytes.toFixed(2)
+    bytes = bytes.toFixed(2);
   }
 
-  return bytes + ' ' + symbols[i]
+  return bytes + ' ' + symbols[i];
 }
 
 /**
@@ -94,10 +96,10 @@ export function onceSession(key, val) {
  * @returns {{}|any}
  */
 export function paramObj(url) {
-  if (!url) return {}
+  if (!url) return {};
 
-  const search = url.split('?')[1]
-  if (!search) return {}
+  const search = url.split('?')[1];
+  if (!search) return {};
 
   return JSON.parse(
     '{"' +
@@ -107,7 +109,7 @@ export function paramObj(url) {
         .replace(/=/g, '":"')
         .replace(/\+/g, ' ') +
       '"}'
-  )
+  );
 }
 
 /**
@@ -117,5 +119,103 @@ export function paramObj(url) {
  * @returns {number}
  */
 export function randomNumber(m, n) {
-  return Math.floor(Math.random() * (m - n) + n)
+  return Math.floor(Math.random() * (m - n) + n);
+}
+
+
+// ---------------------------------------------------轮询方法开始
+
+/**
+ * 轮询类封装
+ * @param config 配置对象
+ * @constructor
+ */
+ export function Thread(config) {
+  this.params = this.init(config);
+}
+/**
+ * 初始化对象参数
+ * @param config 配置对象
+ */
+Thread.prototype.init = function (config) {
+  const params = config || {
+        start () {},
+        stop () {},
+        number: 0,
+        time: 300,
+      };
+  params.start = params.start ? params.start : function () {};
+  params.stop = params.stop ? params.stop : function () {};
+  params.number = params.number ? Math.abs(parseInt(params.number)) : 0;
+  params.time = params.time ? Math.abs(parseInt(params.time)) : 300;
+  this.time = params.time;
+  return params;
+};
+/**
+ * 轮询实现
+ * @param start
+ * @param stop
+ * @param number
+ * @param time
+ */
+Thread.prototype.run = function () {
+  this.timer = setTimeout(this.runTime.bind(this), this.time);
+};
+
+/**
+ * 执行过程函数
+ */
+Thread.prototype.runTime = function () {
+  try {
+    this.params.start();
+  } finally {
+    if (!this.params.number === 0) {
+      if (this.total >= this.params.number) {
+        this.params.stop();
+        clearTimeout(this.timer);
+        return;
+      }
+      if (!this.total) {
+        this.total = 1;
+      }
+      this.total++;
+    }
+    clearTimeout(this.timer);
+  }
+  this.timer = setTimeout(this.runTime.bind(this), this.time);
+};
+
+/**
+ * 停止轮询
+ */
+Thread.prototype.stop = function () {
+  clearTimeout(this.timer);
+  this.params.stop();
+};
+
+// ---------------------------------------------------轮询方法结束
+
+/**
+ * 倒计时方法，可用作手机验证码的倒计时
+ * @param {*} _e 传this
+ * @param {Number} _duration 倒计时的时长(秒数)
+ * @param {String} _timeParam 需要倒计时的参数名字
+ */
+ export function countDown(e, duration, timeParam) {
+  if (!e.countDown_interval) {
+      e[`${timeParam}`] = duration--
+      e.countDown_interval = setInterval(() => {
+          e[`${timeParam}`] = duration--
+          if (duration=== 0) {
+              setTimeout(() => {
+                  clearInterval(e.countDown_interval)
+                  e.countDown_interval = null
+                  e[`${timeParam}`] = null
+              }, 1000);
+          }
+      }, 1000)
+  } else {
+      // eslint-disable-next-line no-useless-return
+      return;
+  }
 }
