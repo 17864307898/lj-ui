9d3389a46b9c48b73a65541c06a7560f40157667
杨雅婷 <yangyating@7-cai.com>
2022-11-24 16:55:37 +0800

    feat(upload): 更新

--- src/lj-upload/demo/index.vue
+++ src/lj-upload/demo/index.vue
@@ -1,21 +1,28 @@
 <template>
-  <lj-upload :upload-parmes="uploadParmes" @uploadInfo="fnUploadInfo" />
+  <lj-upload 
+      ref="ljuploads"
+      :accept="uploadParmes.accept" 
+      :action="uploadParmes.accept"
+      :maxSize="uploadParmes.maxSize"
+      :drag="uploadParmes.drag"
+      :content="uploadParmes.content"
+      :limit="uploadParmes.limit"
+      @uploadInfo="fnUploadInfo" 
+  />
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       uploadParmes: {
-        accept: '*',
+        accept: '',
         uploadPath: '/api/file/upload',
         maxSize: '4294967296',
+        drag: true,
         content: {
           maxSize: '请上传小于4G的文件',
-          againUp: `将文件拖到此处，或<em>重新上传</em>`,
-          clickUp: `将文件拖到此处，或<em>点击上传</em>`,
-        }
+        },
       },
       uploadData: {},
     };
--- src/lj-upload/upload.vue
+++ src/lj-upload/upload.vue
@@ -2,24 +2,34 @@
   <div class="uploadWrap">
     <el-upload
       ref="upload"
-      :accept="uploadParmes.accept"
-      :action="uploadParmes.uploadPath"
-      class="el-upload-dragger"
+      :accept="accept"
+      :headers="headers"
+      :data="data"
+      :action="action"
+      :with-credentials="withCredentials"
+      :show-file-list="showFileList"
+      :before-upload="onBeforeUpload"
+      :class="drag ? 'upload-demo' : 'el-upload-dragger'"
       :file-list="fileList"
-      :on-change="handleSuccess"
+      :limit="limit"
+      :disabled="disabled"
+      :multiple="multiple"
+      :on-change="handleChange"
+      :on-error="handleError"
+      :on-exceed="handleExceed"
       :on-remove="handleRemove"
+      :on-success="handleSuccess"
     >
-      <i class="el-icon-upload"></i>
-      <div
-        class="el-upload__text"
-        v-html="
-          file.size
-            ? uploadParmes.content?.againUp
-            : uploadParmes.content?.clickUp
-        "
-      ></div>
+      <slot name="uploadIcon"><i class="el-icon-upload"></i></slot>
+      <div class="el-upload__text">
+        <slot name="uploadText">
+          <span>将文件拖到此处，或<em>点击上传</em></span>
+        </slot>
+      </div>
       <div slot="tip" class="el-upload__tip">
-        <p>{{ uploadParmes.content?.maxSize }}</p>
+        <p>
+          <slot name="uploadTip">请上传小于4G的文件</slot>
+        </p>
       </div>
     </el-upload>
   </div>
@@ -34,7 +44,57 @@ Vue.use(Upload);
 export default {
   name: 'lj-upload',
   props: {
-    uploadParmes: {
+    accept: {
+      type: String,
+      default: '',
+    },
+    action: {
+      type: String,
+      default: '',
+    },
+    multiple: {
+      type: Boolean,
+      default: false,
+    },
+    drag: {
+      type: Boolean,
+      default: false,
+    },
+    showFileList: {
+      type: Boolean,
+      default: false,
+    },
+    withCredentials: {
+      type: Boolean,
+      default: false,
+    },
+    limit: {
+      type: Number,
+      default: 0,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    headers: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+    data: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+    credentials: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+    content: {
       type: Object,
       default: () => {
         return {};
@@ -45,44 +105,47 @@ export default {
     return {
       file: '',
       fileList: [],
+      againUpload: false,
     };
   },
-  created() {
-    this.$nextTick(() => {
-      if (this.uploadParmes.type === 2) {
-        this.$refs.upload.$children[0].$refs.input.webkitdirectory = true;
-      }
-    });
-  },
   methods: {
-    handleSuccess(file, fileList) {
-      console.log(file)
-      // 上传前判断大小
-      this.onBeforeUpload(file, fileList);
-      // 如果接口返回成功，给父级传参
-      if (file.response) {
-        this.$emit('uploadInfo', file);
-      }
-      this.file = file;
-      // 重传判断
-      if (fileList && fileList.length > 1) {
-        const { uploadFiles } = this.$refs.upload;
-        const index = uploadFiles.indexOf(fileList[0]);
-        uploadFiles.splice(index, 1);
-      }
+    clearFiles() {
+      this.$refs.upload.clearFiles()
     },
-    handleRemove(file) {
-      this.$emit('uploadInfo', {});
-      this.file = file;
+    handleChange(file, fileList) {
+      this.$emit('uploadChange', {file: file, fileList: fileList})
     },
-    onBeforeUpload(file, fileList) {
-      const isLtSize = file.size < this.uploadParmes.maxSize;
+    handleSuccess(res, file, fileList) {
+      if (!this.limit || this.limit < 2) {
+        this.fileList = [file];
+        this.$emit('uploadSuccess', {res: res, file: file, fileList: fileList})
+      } else {
+        this.fileList = fileList;
+        this.$emit('uploadSuccess', {res: res, file: file, fileList: fileList})
+      }
+      this.file = 'file';
+    },
+    handleError() {
+      this.$message({
+        message: this.content.errorMsg ? this.content.errorMsg : '上传失败！',
+        type: 'error',
+        center: true,
+        duration: 1500,
+      });
+    },
+    handleRemove(file, fileList) {
+      this.$emit('uploadRemove', {file: file, fileList: fileList})
+      this.file = file;
+      this.fileList = fileList;
+    },
+    handleExceed() {
+      this.$message.warning(this.content.Exceed? this.content.Exceed : `当前限制选择 ${this.limit} 个文件`);
+    },
+    onBeforeUpload(file) {
+      let maxSize = this.maxSize ? this.maxSize : 4294967296
+      const isLtSize = file.size < maxSize;
       if (!isLtSize) {
-        this.$message.error(this.uploadParmes.content?.maxSize+'！');
-        const { uploadFiles } = this.$refs.upload;
-        const index = uploadFiles.indexOf(fileList[0]);
-        uploadFiles.splice(index, 1);
-        this.handleRemove(file);
+        this.$message.error(this.content.maxSize + '！');
         return false;
       }
     },
