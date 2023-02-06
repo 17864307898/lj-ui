c0b76ffb05a3099ec9cb14fa2bf0b390afc79712
niezhiwei <niezhiwei>
2023-02-06 16:21:47 +0800

    feat(directives): 新增press及trim指令

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,9 +1,13 @@
 import title from './title'
+import press from './press'
+import trim from './trim'
 
-const directives = [title]
+const directives = [title, press, trim]
 
 export {
   title,
+  press,
+  trim,
 }
 
 // 保证可以全局引入及按需引入
